0183be3c0d58e84215317db9e0897f25d737bef8
Paulo Silva <paulofernando78@gmail.com>
2025-05-22 15:54:59 -0300

    wip

---
+++ src/app.js
@@ -0,0 +1,8 @@
+
+window.app = {} // Create the 'app' global object
+app.router = Router // Add Router to 'app' global object
+
+// Guarantee DOM loading before runnning code
+window.addEventListener("DOMContentLoaded", () => {
+  app.router.init(); // Initialize the router
+})
