6dad1f9a9e7c6a07d7e0dbca94d85ac8b563ed05
Paulo Silva <paulofernando78@gmail.com>
2025-05-22 15:29:35 -0300

    wip

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import "./components/index";
 
-// let header = document.querySelector("wc-header");
+let header = document.querySelector("wc-header");
 
-// const login = header.shadowRoot.querySelector("[data-icon=login]")
-// login.addEventListener("click", () => {
-//   const layout = document.createElement("wc-layout");
-//   document.body.appendChild(layout)
-// })
+const login = header.shadowRoot.querySelector("[data-icon=login]")
+login.addEventListener("click", () => {
+  const layout = document.createElement("wc-layout");
+  document.body.appendChild(layout)
+})
 
-// const logout = header.shadowRoot.querySelector("[data-icon=logout]");
-// logout.style.display = "none"
+const logout = header.shadowRoot.querySelector("[data-icon=logout]");
+logout.style.display = "none"
 
-// const menu = header.shadowRoot.querySelector("[data-icon=menu]")
-// menu.style.display = "none";
+const menu = header.shadowRoot.querySelector("[data-icon=menu]")
+menu.style.display = "none";
 
 const phrases = document.querySelector("#home-phrases-display");
 
