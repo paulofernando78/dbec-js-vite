72660228fd6aa0a79654867d88555e9fb041eaa1
Paulo Silva <paulofernando78@gmail.com>
2025-07-01 19:02:12 -0300

    wip

--- src/components/molecules/Exercise.js
+++ src/components/molecules/Exercise.js
@@ -26,10 +26,12 @@ class Exercise extends HTMLElement {
       titleDescWrapper.style.marginBottom = "var(--break-line)";
       container.appendChild(titleDescWrapper);
 
-      const title = document.createElement("p");
-      title.textContent = item.title;
-      title.style.fontWeight = "bold";
-      titleDescWrapper.appendChild(title);
+      if (item.title) {
+        const title = document.createElement("p");
+        title.textContent = item.title;
+        title.style.fontWeight = "bold";
+        titleDescWrapper.appendChild(title);
+      }
 
       if (item.description) {
         const description = document.createElement("p");
@@ -39,105 +41,108 @@ class Exercise extends HTMLElement {
       }
 
       // Question
-      const question = document.createElement("p");
+      if (item.question) {
+        const question = document.createElement("p");
+        item.question.forEach((q) => {
+          if (q.bold) {
+            const questionBold = document.createElement("span");
+            questionBold.textContent = q.bold;
+            questionBold.style.fontWeight = "bold";
+            question.appendChild(questionBold);
+          }
+          if (q.text) {
+            const questionText = document.createElement("span");
+            questionText.textContent = q.text;
+            question.appendChild(questionText);
+          }
 
-      item.question.forEach((q) => {
-        if (q.bold) {
-          const questionBold = document.createElement("span");
-          questionBold.textContent = q.bold;
-          questionBold.style.fontWeight = "bold";
-          question.appendChild(questionBold);
-        }
-        if (q.text) {
-          const questionText = document.createElement("span");
-          questionText.textContent = q.text;
-          question.appendChild(questionText);
-        }
-
-        if (q.mark) {
-          const questionMark = document.createElement("mark");
-          questionMark.textContent = q.mark;
-          question.appendChild(questionMark);
-        }
-      });
-
-      container.appendChild(question);
-
-      item.options.forEach((option) => {
-        const optionWrapper = document.createElement("div");
-        optionWrapper.style.display = "flex";
-        optionWrapper.style.alignItems = "start";
-        optionWrapper.style.gap = "8px";
-        optionWrapper.style.marginTop = "10px";
-
-        // input
-        const input = document.createElement("input");
-        input.style.appearance = "none";
-        input.style.WebkitAppearance = "none"; // ???
-        input.style.MozAppearance = "none";
-
-        input.style.backgroundColor = "var(--button-color)";
-        input.style.boxShadow = "var(--neumorphism)";
-        input.style.cursor = "pointer";
-        input.style.minWidth = "20px";
-        input.style.minHeight = "20px";
-        input.style.borderRadius = "50%";
-        input.style.marginTop = "1px";
-        input.style.marginLeft = "3px";
-
-        input.style.position = "relative";
-        input.style.bottom = "1px";
-
-        input.addEventListener("mousedown", () => {
-          input.style.boxShadow = "var(--neumorphism-active)";
-          input.style.transform = "scale(0.95)";
+          if (q.mark) {
+            const questionMark = document.createElement("mark");
+            questionMark.textContent = q.mark;
+            question.appendChild(questionMark);
+          }
         });
+        container.appendChild(question);
+      }
 
-        input.addEventListener("mouseup", () => {
+
+      if (item.options) {
+        item.options.forEach((option) => {
+          const optionWrapper = document.createElement("div");
+          optionWrapper.style.display = "flex";
+          optionWrapper.style.alignItems = "start";
+          optionWrapper.style.gap = "8px";
+          optionWrapper.style.marginTop = "10px";
+
+          // input
+          const input = document.createElement("input");
+          input.style.appearance = "none";
+          input.style.WebkitAppearance = "none"; // ???
+          input.style.MozAppearance = "none";
+
+          input.style.backgroundColor = "var(--button-color)";
           input.style.boxShadow = "var(--neumorphism)";
-          input.style.transform = "scale(1)";
+          input.style.cursor = "pointer";
+          input.style.minWidth = "20px";
+          input.style.minHeight = "20px";
+          input.style.borderRadius = "50%";
+          input.style.marginTop = "1px";
+          input.style.marginLeft = "3px";
+
+          input.style.position = "relative";
+          input.style.bottom = "1px";
+
+          input.addEventListener("mousedown", () => {
+            input.style.boxShadow = "var(--neumorphism-active)";
+            input.style.transform = "scale(0.95)";
+          });
+
+          input.addEventListener("mouseup", () => {
+            input.style.boxShadow = "var(--neumorphism)";
+            input.style.transform = "scale(1)";
+          });
+
+          // dot
+          const dot = document.createElement("span");
+          dot.style.position = "absolute";
+          dot.style.top = "2px";
+          dot.style.left = "5px";
+          dot.style.width = "15px";
+          dot.style.height = "15px";
+          dot.style.borderRadius = "50%";
+          dot.style.backgroundColor = "#A8A8A8";
+          dot.style.display = "none";
+
+          input.style.position = "relative";
+
+          input.addEventListener("change", () => {
+            const container = input.closest(".radio-exercise-group");
+            const alldots = container.querySelectorAll(".radio-dot");
+            alldots.forEach((p) => (p.style.display = "none"));
+            dot.style.display = "block";
+          });
+
+          dot.classList.add("radio-dot");
+          optionWrapper.style.position = "relative";
+          optionWrapper.appendChild(dot);
+
+          input.type = "radio";
+          input.name = `radio-${idx}`;
+          input.value = option.option;
+          optionWrapper.appendChild(input);
+
+          const label = document.createElement("label");
+          label.textContent = option.option;
+          optionWrapper.appendChild(label);
+
+          const result = document.createElement("span");
+          result.classList.add("result");
+          result.style.display = "none";
+          label.appendChild(result);
+
+          container.appendChild(optionWrapper);
         });
-
-        // dot
-        const dot = document.createElement("span");
-        dot.style.position = "absolute";
-        dot.style.top = "2px";
-        dot.style.left = "5px";
-        dot.style.width = "15px";
-        dot.style.height = "15px";
-        dot.style.borderRadius = "50%";
-        dot.style.backgroundColor = "#A8A8A8";
-        dot.style.display = "none";
-
-        input.style.position = "relative";
-
-        input.addEventListener("change", () => {
-          const container = input.closest(".radio-exercise-group");
-          const alldots = container.querySelectorAll(".radio-dot");
-          alldots.forEach((p) => (p.style.display = "none"));
-          dot.style.display = "block";
-        });
-
-        dot.classList.add("radio-dot");
-        optionWrapper.style.position = "relative";
-        optionWrapper.appendChild(dot);
-
-        input.type = "radio";
-        input.name = `radio-${idx}`;
-        input.value = option.option;
-        optionWrapper.appendChild(input);
-
-        const label = document.createElement("label");
-        label.textContent = option.option;
-        optionWrapper.appendChild(label);
-
-        const result = document.createElement("span");
-        result.classList.add("result");
-        result.style.display = "none";
-        label.appendChild(result);
-
-        container.appendChild(optionWrapper);
-      });
+      }
 
       this.shadowRoot.appendChild(container);
     });
@@ -148,6 +153,7 @@ class Exercise extends HTMLElement {
     buttonsWrapper.style.marginLeft = "3px";
     buttonsWrapper.style.marginBlock = ".625rem";
 
+    // Check answers
     const checkAnswersButton = document.createElement("wc-button");
     checkAnswersButton.setAttribute("data-icon", "check");
     checkAnswersButton.addEventListener("click", () => {
@@ -189,6 +195,7 @@ class Exercise extends HTMLElement {
     // const showAnswersButton = document.createElement("wc-button");
     // showAnswersButton.setAttribute("data-icon", "visibility");
 
+    // Reset button
     const resetButton = document.createElement("wc-button");
     resetButton.setAttribute("data-icon", "reset");
     resetButton.addEventListener("click", () => {
