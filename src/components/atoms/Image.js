6e454e9ff8eb6160c25466d4664043160262255b
Paulo Silva <paulofernando78@gmail.com>
2025-07-24 10:27:07 -0300

    wip

--- src/components/atoms/Image.js
+++ src/components/atoms/Image.js
@@ -7,19 +7,39 @@ class Image extends HTMLElement {
   }
 
   set data(img) {
-    // CSS imports
     const cssImports = document.createElement("style");
     cssImports.textContent = cssImportsPath;
     this.shadowRoot.appendChild(cssImports);
 
+    const wrapper = document.createElement("div");
+    wrapper.style.position = "relative";
+    wrapper.style.display = "inline-block";
+
     const image = document.createElement("img");
+    image.style.position = "relative";
     image.style.width = img.width || "100%";
     image.style.border = "var(--img-border)";
     image.style.borderRadius = "var(--border-radius)";
     image.src = img.src;
     image.alt = img.alt || "";
+    wrapper.appendChild(image);
 
-    this.shadowRoot.appendChild(image);
+    if (img.number) {
+      const number = document.createElement("span");
+      number.style.position = "absolute";
+      number.style.border = "1px solid black";
+      number.style.background = "white",
+      number.style.fontFamily = "courier",
+      number.style.fontSize = ".75rem",
+      number.style.borderRadius = "4px",
+      number.style.padding = "2px 5px",
+      number.style.top = "8px",
+      number.style.left = "8px",
+      number.textContent = img.number;
+      wrapper.appendChild(number);
+    }
+
+    this.shadowRoot.appendChild(wrapper);
   }
 }
 
