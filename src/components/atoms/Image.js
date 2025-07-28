d688b53f24967b11b809210864a98353a82c4a98
Paulo Silva <paulofernando78@gmail.com>
2025-07-28 01:21:42 -0300

    wip

--- src/components/atoms/Image.js
+++ src/components/atoms/Image.js
@@ -11,13 +11,31 @@ class Image extends HTMLElement {
     cssImports.textContent = cssImportsPath;
     this.shadowRoot.appendChild(cssImports);
 
+    const css = document.createElement("style"); /*css*/
+    css.textContent = `
+     img {
+        height: auto;
+      }
+
+      @media (max-width: 500px) {
+        img {
+          width: 100% !important;
+        }
+    }
+  }
+    `;
+    this.shadowRoot.appendChild(css);
+
     const wrapper = document.createElement("div");
     wrapper.style.position = "relative";
     wrapper.style.display = "inline-block";
 
     const image = document.createElement("img");
     image.style.position = "relative";
-    image.style.width = img.width || "100%";
+    if (img.width) {
+      image.style.setProperty("width", img.width);
+    }
+    image.style.margin = "0 auto";
     image.style.border = "var(--img-border)";
     image.style.borderRadius = "var(--border-radius)";
     image.src = img.src;
@@ -28,13 +46,13 @@ class Image extends HTMLElement {
       const number = document.createElement("span");
       number.style.position = "absolute";
       number.style.border = "1px solid black";
-      number.style.background = "white",
-      number.style.fontFamily = "courier",
-      number.style.fontSize = ".75rem",
-      number.style.borderRadius = "4px",
-      number.style.padding = "2px 5px",
-      number.style.top = "8px",
-      number.style.left = "8px",
+      number.style.background = "white";
+      number.style.fontFamily = "courier";
+      number.style.fontSize = ".75rem";
+      number.style.borderRadius = "4px";
+      number.style.padding = "2px 5px";
+      number.style.top = "8px";
+      number.style.left = "8px";
       number.textContent = img.number;
       wrapper.appendChild(number);
     }
