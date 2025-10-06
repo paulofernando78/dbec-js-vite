f75e141500c61bd0c999c905e3443a800aa88f89
Paulo Silva <paulofernando78@gmail.com>
2025-10-06 03:14:16 -0300

    wip

--- src/components/organisms/Layout.js
+++ src/components/organisms/Layout.js
@@ -12,44 +12,6 @@ class Layout extends HTMLElement {
       this.shadowRoot.appendChild(style);
     });
 
-    const style = document.createElement("style"); /*css*/
-    style.textContent = `
-      .layout {
-        display: grid;
-        grid-template-columns: auto 1fr;
-        grid-template-rows: auto 1fr auto;
-        gap: 10px;
-        height: calc(100dvh - 20px)
-      }
-
-      .grid-span {
-        grid-column: 1 / 3
-      }
-
-      #app {
-        grid-column: 2;
-        grid-row: 2;
-        overflow-y: auto;
-        padding-right: 8px
-      }
-
-      @media (max-width: 768px) {
-        .layout {
-          display: flex;
-          flex-direction: column
-        }
-
-        wc-nav-bar {
-          display: none;
-        }
-
-        wc-nav-bar.open {
-          display: block;
-        }
-      }
-    `;
-    this.shadowRoot.appendChild(style);
-
     const layout = document.createElement("div");
     layout.classList.add("layout");
 
@@ -67,18 +29,18 @@ class Layout extends HTMLElement {
     layout.appendChild(app);
 
     const scrollTop = document.createElement("wc-scroll-top");
-    this.shadowRoot.appendChild(scrollTop);
+    layout.appendChild(scrollTop);
 
     // Referência ao container com scroll
     const scrollContainer = app; // já criado anteriormente
 
     scrollContainer.addEventListener("scroll", () => {
-  if (scrollContainer.scrollTop > 300) {
-    scrollTop.classList.add("visible");
-  } else {
-    scrollTop.classList.remove("visible");
-  }
-});
+      if (scrollContainer.scrollTop > 300) {
+        scrollTop.classList.add("visible");
+      } else {
+        scrollTop.classList.remove("visible");
+      }
+    });
 
     scrollTop.addEventListener("click", () => {
       app.scrollTo({ top: 0, behavior: "smooth" });
