4b985a3801ad497a8226a775ffe6f89d6559e8c8
Paulo Silva <paulofernando78@gmail.com>
2025-10-11 04:55:09 -0300

    wip

--- src/js/three-js/index-scene.js
+++ src/js/three-js/index-scene.js
@@ -1,7 +1,32 @@
 import * as THREE from "three";
 
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
 const renderer = new THREE.WebGLRenderer();
+renderer.setAnimationLoop(animate);
+const container = document.getElementById("three-js-logo");
+const width = container.clientWidth || 400;
+const height = container.clientHeight || 300;
+renderer.setSize(width, height);
 
-renderer.setSize(window.innerWidth, window.innerHeight);
+container.appendChild(renderer.domElement);
 
-document.body.appendChild(renderer.domElement)
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
