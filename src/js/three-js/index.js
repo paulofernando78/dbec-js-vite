99f5dc857a6fc9a527c789b98658b24bca0dff70
Paulo Silva <paulofernando78@gmail.com>
2025-10-12 01:30:39 -0300

    wip

--- src/js/three-js/index.js
+++ src/js/three-js/index.js
@@ -2,12 +2,20 @@ import "/src/css/three-js-logo.css";
 
 import * as THREE from "three";
 
+const phraseList = [
+  "Hi there!",
+  "How are you doing?",
+  "How's it going?",
+  "What's up?",
+  "How are you?",
+  "How are you feeling?",
+];
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setClearColor(0x000000, 0);
-renderer.setAnimationLoop(animate);
 
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.VSMShadowMap;
@@ -20,13 +28,33 @@ renderer.setSize(container.clientWidth, container.clientHeight);
 
 function createTextTexture(text) {
   const canvas = document.createElement("canvas");
-  const ctx = canvas.getContext("2d")
+  const ctx = canvas.getContext("2d");
+  canvas.width = 512;
+  canvas.height = 512;
+  
+  // Transparent background
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "#111"
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+  
+  ctx.font = "bold 48px Poppins";
+  ctx.fillStyle = "#00ffcc";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  ctx.fillText(text, canvas.width / 2, canvas.height / 2);
+  
+  return new THREE.CanvasTexture(canvas);
 }
+
+// Criar um metrial por face
+const materials = phraseList.map(
+  (phrase) => new THREE.MeshBasicMaterial({ map: createTextTexture(phrase) })
+);
 
 // Material compatible with light
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
+const cube = new THREE.Mesh(geometry, materials);
 scene.add(cube);
 
 const planeGeometry = new THREE.PlaneGeometry(10, 10);
@@ -55,12 +83,14 @@ directional.shadow.blurSamples = 16;
 
 scene.add(ambient, directional);
 
-camera.position.z = 2;
+camera.position.z = 1.5;
 
 function animate() {
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
   cube.castShadow = true;
-
+  
   renderer.render(scene, camera);
 }
+renderer.setAnimationLoop(animate);
+
